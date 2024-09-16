bffe56e47b47c8b5b08223a782141fd71fe47000
Basem Barakat-Allah <basembaraka84521@gmail.com>
2024-09-16 18:09:40 +0300

    newsletter

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -4,26 +4,28 @@ import { NewsletterLogos } from "../utils/icons.util";
 export default function Newsletter() {
   return (
     <div className="newsletter">
-      <div className="content">
-        <h3>
-          Subscribe to our newsletter
-        </h3>
-        <p>
-          Praesent fringilla erat a lacinia egestas. 
-          Donec vehicula tempor libero et cursus. 
-          Donec non quam urna. 
-          Quisque vitae porta ipsum.
-        </p>
-      </div>
-      <div className="inputfield">
-        <input type="text" placeholder="Email address"/>
-        <button>
-          <span>Subscribe</span>
-          <ArrowRight className="arrowright" />
-        </button>
-      </div>
-      <div className="companylogo">
-        <NewsletterLogos />
+      <div className="container">
+        <div className="content">
+          <h3>
+            Subscribe to our newsletter
+          </h3>
+          <p>
+            Praesent fringilla erat a lacinia egestas. 
+            Donec vehicula tempor libero et cursus. 
+            Donec non quam urna. 
+            Quisque vitae porta ipsum.
+          </p>
+        </div>
+        <div className="inputfield">
+          <input type="text" placeholder="Email address"/>
+          <button>
+            <span>Subscribe</span>
+            <ArrowRight className="arrowright" />
+          </button>
+        </div>
+        <div className="companylogo">
+          <NewsletterLogos />
+        </div>
       </div>
     </div>
   );
